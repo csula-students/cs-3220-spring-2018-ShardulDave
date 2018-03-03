1b0b1e9fad8246785776ac27e80c142643346292
Shardu Dave <daveshardul@rocketmail.com>
2018-03-03 12:04:33 -0800

    Commit

--- lab3/dest/app.bundle.js
+++ lab3/dest/app.bundle.js
@@ -731,17 +731,57 @@ function deepCopy(obj) {
 
 
 Object.defineProperty(exports, "__esModule", {
-	value: true
+  value: true
 });
 exports.default = reducer;
 function reducer(state, action) {
-	switch (action.type) {
-		case 'EXAMPLE_MUTATION':
-			state.example = action.payload;
-			return state;
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case 'EXAMPLE_MUTATION':
+      state.example = action.payload;
+      return state;
+    case 'BUY_GENERATOR':
+      for (let i = 0; i < state.generators.length; i++) {
+        let generators = state.generators[i];
+        if (generators.name === action.payload.name) {
+          let gen = new Generator(generator);
+          state.counter -= gen.getCost();
+          generator.quantity++;
+          generator.unlockValue = gen.getCost();
+          return state;
+        }
+      }
+      let generatorName = action.payload.name;
+      const generatorData = {
+        type: 'autonomous',
+        name: generatorName,
+        description: 'desc',
+        quantity: 0,
+        baseCost: 0,
+        unlockValue: 0
+      };
+
+      if (generatorName === "Salary") {
+        generatorData.rate = 5;
+        generatorData.baseCost = 10;
+        generatorData.unlockValue = 10;
+      } else if (generatorName === "Rent") {
+        generatorData.rate = 10;
+        generatorData.baseCost = 100;
+        generatorData.unlockValue = 100;
+      } else if (generatorName === "Share") {
+        generatorData.rate = 20;
+        generatorData.baseCost = 500;
+        generatorData.unlockValue = 500;
+      }
+      generatorData.quantity++;
+      state.counter -= generatorData.baseCost;
+      let instanceofgenerator = new Generator(generatorData);
+      instanceofgenerator.unlockValue = instanceofgenerator.getCost();
+      state.generators.push(instanceofgenerator);
+      return state;
+    default:
+      return state;
+  }
 }
 
 /***/ }),
@@ -873,11 +913,46 @@ exports.default = function (store) {
 			// TODO: render generator initial view
 
 			// TODO: subscribe to store on change event
-
+			this.onStateChange = this.handleStateChange.bind(this);
+			this.store.subscribe(state => {
+				console.log(state);
+			});
 			// TODO: add click event
+			this.addEventListener('click', () => {
+				this.store.dispatch({
+					type: 'BUY GENERATOR',
+					payload: {
+						quantity: 1,
+						name: this.dataset.name
+					}
+				});
+			});
 		}
+
+		handleStateChange(newState) {
+			var g = newState.generators;
+			g.forEach(element => {
+				if (element.name === this.dataset.name) {
+					this.querySelector('.count_ofgen').textContent = element.quantity;
+					this.querySelector('.btnres').textContent.element.unlockValue;
+				}
+			});
+		}
+
+		connectedCallback() {
+			console.log('ExampleComponent#onConnectedCallback');
+			this.store.subscribe(this.onStateChange);
+		}
+
+		disconnectedCallback() {
+			console.log('ExampleComponent#onDisconnectedCallback');
+			this.store.unsubscribe(this.onStateChange);
+		}
+
 	};
 };
+
+window.customElements.define('game-generator', RGBSquareComponent(store));
 
 /***/ }),
 /* 11 */
