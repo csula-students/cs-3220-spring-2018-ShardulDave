4006b7d512155811de1eb3147afd81bdf613ca47
Shardul Dave <daveshardul@rocketmail.com>
2018-04-21 20:54:26 -0700

    Commit

--- homework2/cookie-clicker-lab4-7/src/main/java/edu/csula/web/AuthenticationServlet.java
+++ homework2/cookie-clicker-lab4-7/src/main/java/edu/csula/web/AuthenticationServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/auth")
+@WebServlet("/admin/auth")
 public class AuthenticationServlet extends HttpServlet {
 	@Override
 	public void doGet( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
